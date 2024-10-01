bbdca16b48140a87cf7cffef39218a2a6e4592da
Aanchal <anchal.verma@ibyteinfomatics.in>
2024-10-01 17:24:21 +0530

    RootSaga file added

--- src/Store/Saga/MaincategorySaga.jsx
+++ src/Store/Saga/MaincategorySaga.jsx
@@ -1,6 +1,29 @@
-import { createData } from "./Services/MaincategoryService";
-import { put } from "redux-saga";
+import { put, takeEvery } from "redux-saga/effects"
+import {createData, deleteData, getData, updateData} from "./Services/MaincategoryService"
+import { ADD_MAINCATEGORY, ADD_MAINCATEGORY_RED, DELETE_MAINCATEGORY, DELETE_MAINCATEGORY_RED, GET_MAINCATEGORY, GET_MAINCATEGORY_RED, UPDATE_MAINCATEGORY, UPDATE_MAINCATEGORY_RED } from "../Constant"
+function* createSaga(payload){
+    var response = yield createData(payload)
+    put({type : ADD_MAINCATEGORY_RED, payload : response})
+}
 
-function createSaga(payload){
-    var response = createData(payload)
+function* getSaga(){
+    var response = yield getData()
+    put({type : GET_MAINCATEGORY_RED, payload : response})
+}
+
+function* updateSaga(payload){
+    var response = yield updateData(payload)
+    put({type : UPDATE_MAINCATEGORY_RED, payload : response})
+}
+
+function* deleteSaga(payload){
+    yield deleteData(payload)
+    put({type : DELETE_MAINCATEGORY_RED,payload})
+}
+
+export default function* maincategorySaga(){
+    yield takeEvery(ADD_MAINCATEGORY,createSaga),
+    yield takeEvery(GET_MAINCATEGORY,getSaga),
+    yield takeEvery(UPDATE_MAINCATEGORY,updateSaga),
+    yield takeEvery(DELETE_MAINCATEGORY,deleteSaga)
 }
